4be0e664af993c2be7be970a8b3db32efbccaaab
cristianManco <camilomanco2005@gmail.com>
2024-05-30 16:40:18 -0500

    Add: entities of the author, book and sales modules of the project with their respective relationships

---
+++ src/author/entities/author.entity.ts
@@ -0,0 +1,36 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, DeleteDateColumn, ManyToMany, JoinTable } from 'typeorm';
+import { Book } from 'src/books/entities/book.entity';
+
+@Entity()
+export class Author {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  nombre: string;
+
+  @Column()
+  apellido: string;
+
+  @Column({ type: 'date', nullable: true })
+  fecha_nacimiento?: Date;
+
+  @Column({ nullable: true })
+  nacionalidad?: string;
+
+  @Column({ type: 'text', nullable: true })
+  biografia?: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt?: Date;
+
+  @ManyToMany(() => Book, book => book.autores)
+  @JoinTable()
+  books: Book[];
+}
---
+++ src/sales/sale/sale.entity.ts
@@ -0,0 +1,29 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, ManyToOne } from 'typeorm';
+import { Book } from 'src/books/entities/book.entity';
+
+@Entity()
+export class Sale {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'timestamp' })
+  fecha: Date;
+
+  @Column()
+  cliente: string;
+
+  @Column({ type: 'int' })
+  cantidad: number;
+
+  @Column({ type: 'decimal' })
+  total: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @ManyToOne(() => Book, book => book.ventas)
+  libro: Book;
+}
